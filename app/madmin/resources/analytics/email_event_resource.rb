16e6a000affefcc3dbdef52f80695b539bc58d3a
moveson <mark.oveson@gmail.com>
2026-04-10 21:46:46 -0400

    Improve madmin email event resource

--- app/madmin/resources/analytics/email_event_resource.rb
+++ app/madmin/resources/analytics/email_event_resource.rb
@@ -1,30 +1,25 @@
 module Analytics
   class EmailEventResource < Madmin::Resource
     # Attributes
-    attribute :id, form: false, index: true
+    attribute :id, form: false
     attribute :email, index: true
-    attribute :timestamp, index: true
-    attribute :smtp_id, index: false
     attribute :event, index: true
+    attribute :timestamp, index: true
+    attribute :status, index: true
+    attribute :reason, index: true
     attribute :type, index: true
+    attribute :response, index: false
+    attribute :ip, index: false
+    attribute :useragent, index: false
+    attribute :smtp_id, index: false
     attribute :category, index: false
+    attribute :event_type, index: false
     attribute :provider_event_id, index: false
     attribute :provider_message_id, index: false
-    attribute :reason, index: false
-    attribute :status
-    attribute :ip, index: false
-    attribute :response, index: false
-    attribute :event_type, index: false
-    attribute :useragent, index: false
     attribute :created_at, form: false, index: true
     attribute :updated_at, form: false, index: false
 
     # Associations
-
-    # Uncomment this to customize the display name of records in the admin area.
-    # def self.display_name(record)
-    #   record.name
-    # end
 
     def self.default_sort_column
       "created_at"
